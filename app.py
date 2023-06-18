54b0b60b27e32bc55f3e104b85f47de45969d7f9
Prakash R <prakashramanukolanu1998@gmail.com>
2023-06-18 12:26:37 +0530

    Get values from submitted form.

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
-app = Flask(__name__)
+app = Flask(__name__)  # app instance
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        first_name = request.form['firstname']
+        last_name = request.form['lastname']
+        email = request.form['email']
+        start_date = request.form['start_date']
+        occupation = request.form['occupation']
+
     return render_template('index.html')
 
 
