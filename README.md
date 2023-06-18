383f49b3196f004bab339f475556c47e78e8b5e2
Prakash R <prakashramanukolanu1998@gmail.com>
2023-06-18 20:20:55 +0530

    Send mail when the form is submitted. Add README file.

---
+++ README.md
@@ -0,0 +1,4 @@
+# Project Description
+
+## A simple job application web-form using Flask framework.
+## When the form is submitted, the submitted data in the form will be stored in database and is sent through the email to a receiver.
--- app.py
+++ app.py
@@ -1,12 +1,24 @@
 from flask import Flask, render_template, request, flash
 from flask_sqlalchemy import SQLAlchemy
+from flask_mail import Mail, Message
+import os
 from datetime import datetime
 
 app = Flask(__name__)  # app instance
+
 app.config['SECRET_KEY'] = 'secretkey'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///applicants_data.db'
 
+# mail configuration
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_USERNAME'] = 'my.projects.with.python@gmail.com'
+app.config['MAIL_PASSWORD'] = os.getenv('EMAIL_PASSWORD')
+
 db = SQLAlchemy(app)  # db instance
+
+mail = Mail(app)  # mail instance
 
 
 class FormData(db.Model):
@@ -35,6 +47,19 @@ def index():
                         occupation=occupation)
         db.session.add(form)
         db.session.commit()
+        message_body = f"Hi, You have received a new application.\n\nHere are the details:\n" \
+                       f"Full name : {first_name} {last_name}\n" \
+                       f"Email : {email}\n" \
+                       f"Ready to join from : {start_date}\n" \
+                       f"Current employment status : {occupation}\n\n" \
+                       f"Regards."
+        message = Message(subject='New Application Received',
+                          sender=app.config['MAIL_USERNAME'],
+                          recipients=[app.config['MAIL_USERNAME']],
+                          body=message_body
+                          )
+        mail.send(message)
+
         flash(f"Hi {first_name}, Your data is submitted successfully.", 'success')
         db.session.close()
     return render_template('index.html')
